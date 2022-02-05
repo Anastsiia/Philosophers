e360dfb116168dafa4ec85dd6952d42362cbf3bc
Anastasiia Pimkina <78366371+Anastsiia@users.noreply.github.com>
2022-02-05 15:44:46 +0300

    Update bonus.h

--- bonus.h
+++ bonus.h
@@ -6,7 +6,7 @@
 /*   By: ctifany <ctifany@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 10:37:37 by ctifany           #+#    #+#             */
-/*   Updated: 2022/02/05 11:06:03 by ctifany          ###   ########.fr       */
+/*   Updated: 2022/02/05 15:44:23 by ctifany          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,26 @@ typedef struct s_game
 	sem_t			*write;
 	sem_t			*kill;
 }					t_game; //t_philo_info;
+
+int				ft_atoi(char *s);
+int				ft_check_args(t_game *game);
+int				ft_msg(int msg, t_philo *philo);
+int				ft_message(int msg, t_philo *philo);
+unsigned long	ft_time(void);
+
+
+void			ft_create_p(t_game *game);
+void			ft_kill_pid(t_game *game, int kill_pid, int i, int *pid);
+void			ft_fork(t_game *game, int *pid);
+void			ft_init_s(t_game *game);
+void			ft_close_s(t_game *game);
+
+void			ft_live_loop(t_philo *philo);
+void			*ft_live(t_game *game, int i);
+void			*ft_check_live(t_philo *philo);
+
+int				ft_get(int argc, char **argv, t_game *game);
+void			ft_usleep(int wait);
 
 #endif
 
